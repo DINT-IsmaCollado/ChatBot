e4f076ceeec122956bcfcbbd224826ccfea42f8b
IsmaCollado <ismael.collado@iesdoctorbalmis.com>
2019-12-10 12:43:14 +0100

    Agregar archivos de proyecto.

---
+++ ChatBot/CustomCommands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatBot
+{
+    class CustomCommands
+    {
+        public static readonly RoutedUICommand OpenSettings = new RoutedUICommand
+            (
+                "OpenSettings",
+                "OpenSettings",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.C,ModifierKeys.Control)
+                }
+            );
+
+    }
+}
